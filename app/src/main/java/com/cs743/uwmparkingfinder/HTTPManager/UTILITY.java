9cab010a5e992b5b9869de7807b9ef2e76a009c6
fbgrecojr <fbgrecojr@me.com>
2015-11-21 22:59:29 -0600

    some comments

--- app/src/main/java/com/cs743/uwmparkingfinder/HTTPManager/UTILITY.java
+++ app/src/main/java/com/cs743/uwmparkingfinder/HTTPManager/UTILITY.java
@@ -1,9 +1,10 @@
 package com.cs743.uwmparkingfinder.HTTPManager;
 
 /**
- * Created by MOM on 11/21/15.
+ * Created by fbgrecojr on 11/21/15.
  */
 public class UTILITY {
 
     public static final String UBUNTU_SERVER_URL ="http://ec2-54-152-4-103.compute-1.amazonaws.com/scripts.php";
+
 }
